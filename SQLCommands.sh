136e502e50098662b72acaeed580d8decdb37552
Christian Martinez <crismartine.e@gmail.com>
2022-07-30 18:03:20 -0500

    Installed MySQL and configured it

---
+++ SQLCommands.sh
@@ -0,0 +1,10 @@
+# To install MySQL
+sudo apt update-get -y && sudo apt-get upgrade -y
+sudo apt install mysql-server
+
+# To open MYSQL
+sudo mysql
+
+# References:
+# MySQL installation and config https://www.digitalocean.com/community/tutorials/how-to-install-mysql-on-ubuntu-20-04-es
+# MySQL cheatsheet: https://devhints.io/mysql
